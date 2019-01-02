e5d917c7cf9b466555bffeee5878513f3513c3e5
brian-quach <briiquach@gmail.com>
2019-01-02 15:19:49 -0500

    Rename

---
+++ 2048Tetris/Assets/Scripts/Grid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public static int width = 10;
+    public static int height = 20;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
