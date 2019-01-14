99affc0aea0b4082bb3c5c67a03c288b2744ae39
brian-quach <briiquach@gmail.com>
2019-01-14 14:49:41 -0500

    Added text to show scores

--- 2048Tetris/Assets/Scripts/Tile.cs
+++ 2048Tetris/Assets/Scripts/Tile.cs
@@ -6,14 +6,22 @@ public class Tile : MonoBehaviour {
 
     public int score = 2;
     public int posID = -1;
-    
-	// Use this for initialization
-	void Start () {
+
+    private TextMesh textMesh;
+
+    private void Awake() {
+        textMesh = transform.GetChild(0).gameObject.GetComponent<TextMesh>();
+        textMesh.text = score.ToString();
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        textMesh.text = score.ToString();
+        textMesh.gameObject.transform.rotation = Quaternion.identity;
+    }
 }
